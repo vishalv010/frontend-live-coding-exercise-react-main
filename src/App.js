6ba9f119e27108d5cd843ed2380cec1108e4f665
vishalv010 <paonta.vishu@gmail.com>
2024-04-30 21:37:51 +0530

    added few files

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import {QUESTIONS} from "./questions";
+
+class App extends Component {
+  state = {
+  };
+
+
+  render() {
+    return (
+      <div className="main__wrap">
+        <main className="container">
+          <div>
+            TODO
+          </div>
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
